fe62ba2d01a7c28ce33df4bb52b6b1c128df2f3c
MontasserBH <montasserb94@gmail.com>
2018-02-23 11:55:05 +0100

    ajout de boussole

--- app/src/main/java/com/m2dl/mobebmp/mobe_bmp/Home.java
+++ app/src/main/java/com/m2dl/mobebmp/mobe_bmp/Home.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
@@ -13,9 +14,16 @@ import org.osmdroid.config.Configuration;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.views.overlay.MinimapOverlay;
+import org.osmdroid.views.overlay.ScaleBarOverlay;
+import org.osmdroid.views.overlay.compass.CompassOverlay;
+import org.osmdroid.views.overlay.compass.InternalCompassOrientationProvider;
+import org.osmdroid.views.overlay.gestures.RotationGestureOverlay;
+import org.osmdroid.views.overlay.gridlines.LatLonGridlineOverlay2;
 
 public class Home extends AppCompatActivity {
     MapView mMapView = null;
+    private CompassOverlay mCompassOverlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +46,11 @@ public class Home extends AppCompatActivity {
         mapController.setZoom(17);
         GeoPoint startPoint = new GeoPoint(43.5610329,1.4720187);
         mapController.setCenter(startPoint);
+
+        this.mCompassOverlay = new CompassOverlay(ctx, new InternalCompassOrientationProvider(ctx), mMapView);
+        this.mCompassOverlay.enableCompass();
+        this.mCompassOverlay.setCompassCenter(40, 500);
+        mMapView.getOverlays().add(this.mCompassOverlay);
     }
 
     @Override
